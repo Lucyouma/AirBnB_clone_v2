d8fcc39d6af5100c657d556140ba0ef0ac84fd54
Lucyouma <akinyilucy09@gmail.com>
2024-08-21 17:45:32 -0400

    added render html tmplate with states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+endpoint to render template with states
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    closes Sqlalachemy session
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    renders html page with states
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda key: key.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == "__main__":
+    """
+    Main
+    """
+    app.run(host='0.0.0.0', port=5000)
